eee7e144e8406924a056687949eddedd41856af6
Ari <ari@fullstack.io>
2018-03-29 16:36:23 -0400

    Updated default google maps version to 3.31

--- src/lib/GoogleApi.js
+++ src/lib/GoogleApi.js
@@ -1,48 +1,51 @@
-import invariant from 'invariant'
+import invariant from 'invariant';
 
 export const GoogleApi = function(opts) {
-    opts = opts || {}
+  opts = opts || {};
 
-    invariant(opts.hasOwnProperty('apiKey'),
-              'You must pass an apiKey to use GoogleApi');
+  invariant(
+    opts.hasOwnProperty('apiKey'),
+    'You must pass an apiKey to use GoogleApi'
+  );
 
-    const apiKey = opts.apiKey;
-    const libraries = opts.libraries || ['places'];
-    const client = opts.client;
-    const URL = opts.url || 'https://maps.googleapis.com/maps/api/js';
+  const apiKey = opts.apiKey;
+  const libraries = opts.libraries || ['places'];
+  const client = opts.client;
+  const URL = opts.url || 'https://maps.googleapis.com/maps/api/js';
 
-    const googleVersion = opts.version || '3';
+  const googleVersion = opts.version || '3.31';
 
-    let script = null;
-    let google = window.google || null;
-    let loading = false;
-    let channel = null;
-    let language = opts.language;
-    let region = null;
+  let script = null;
+  let google = window.google || null;
+  let loading = false;
+  let channel = null;
+  let language = opts.language;
+  let region = null;
 
-    let onLoadEvents = [];
+  let onLoadEvents = [];
 
-    const url = () => {
-        let url = URL;
-        let params = {
-            key: apiKey,
-            callback: 'CALLBACK_NAME',
-            libraries: libraries.join(','),
-            client: client,
-            v: googleVersion,
-            channel: channel,
-            language: language,
-            region: region
-        }
+  const url = () => {
+    let url = URL;
+    let params = {
+      key: apiKey,
+      callback: 'CALLBACK_NAME',
+      libraries: libraries.join(','),
+      client: client,
+      v: googleVersion,
+      channel: channel,
+      language: language,
+      region: region
+    };
 
-        let paramStr = Object.keys(params)
-            .filter(k => !!params[k])
-            .map(k => `${k}=${params[k]}`).join('&');
+    let paramStr = Object.keys(params)
+      .filter(k => !!params[k])
+      .map(k => `${k}=${params[k]}`)
+      .join('&');
 
-        return `${url}?${paramStr}`;
-    }
+    return `${url}?${paramStr}`;
+  };
 
-    return url();
-}
+  return url();
+};
 
-export default GoogleApi
+export default GoogleApi;
