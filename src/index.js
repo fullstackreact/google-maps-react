5beca594f76c5144814582669113f0ef8b58fda9
Kilian Ciuffolo <kilian.ciuffolo@gmail.com>
2019-02-02 14:20:56 -0800

    Add backgroundColor to map prop fixes #312

--- src/index.js
+++ src/index.js
@@ -165,7 +165,8 @@ export class Map extends React.Component {
           disableDoubleClickZoom: this.props.disableDoubleClickZoom,
           noClear: this.props.noClear,
           styles: this.props.styles,
-          gestureHandling: this.props.gestureHandling
+          gestureHandling: this.props.gestureHandling,
+          backgroundColor: this.props.backgroundColor
         }
       );
 
@@ -299,7 +300,8 @@ Map.propTypes = {
   noClear: PropTypes.bool,
   styles: PropTypes.array,
   gestureHandling: PropTypes.string,
-  bounds: PropTypes.object
+  bounds: PropTypes.object,
+  backgroundColor: PropTypes.string
 };
 
 evtNames.forEach(e => (Map.propTypes[camelize(e)] = PropTypes.func));
